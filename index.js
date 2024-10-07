0f1d6b067b6126cd2080df7c7a562f0a9e251222
rani-agrawal123 <agrawalrani2003@gmail.com>
2024-10-07 13:50:25 +0530

    number

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+let number = [3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18];
+
+for(i=0; i<number.length; i++){
+    if(number[i]%2 === 0){
+   console.log("Even number");
+    }
+    else{
+   console.log("Odd number");
+    }
+    // console.log(i);
+}
