1826ed91268db5c3f0b40f95a2c0e377826433ca
rani-agrawal123 <agrawalrani2003@gmail.com>
2024-10-12 20:59:50 +0530

    index

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ let number = [3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16,
 
 for(i=0; i<number.length; i++){
     if(number[i]%2 === 0){
-   console.log("Even number");
+   console.log("Even");
     }
     else{
-   console.log("Odd number");
+   console.log("Odd");
     }
     // console.log(i);
 }
